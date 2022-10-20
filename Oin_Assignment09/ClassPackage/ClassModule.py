d2b70c11fb7f6c1a63e3079835d46907ae798cb6
Michael <Michael@Aarons-Laptop.wifi.uc.edu>
2022-10-20 08:58:54 -0400

    Initial Setup

---
+++ Oin_Assignment09/ClassPackage/ClassModule.py
@@ -0,0 +1,8 @@
+# Class Module
+'''
+Name: Oin
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This is where our classes are located
+'''
---
+++ Oin_Assignment09/MainPackage/Main.py
@@ -0,0 +1,8 @@
+# Main
+'''
+Name: Oin
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This is the entry point for our group code
+'''
